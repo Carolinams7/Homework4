98b2f873f83e3b8e20ba6cdc5405dd5ab0b5b23d
Carolinams7 <57420037+Carolinams7@users.noreply.github.com>
2020-01-22 21:52:30 -0600

    hmw4

---
+++ questions.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+question : "Commonly used data types DO NOT include:",
+choiceA : "strings",
+choiceB : "booleans",
+choiceC : "alerts",
+choiceD : "numbers",
+correct : "C"
+    },
+    {
+question : "The condition in an if / else statement is enclosed within ____.",
+choiceA : "quotes",
+choiceB : "curly brackets",
+choiceC : "parentheses",
+choiceD : "square brackets",
+correct : "C"
+    },
+    {
+question : "Arrays in Javascript can be used to store:",
+choiceA : "numbers and strings",
+choiceB : "other arrays",
+choiceC : "boolean",
+choiceD : "all the above",
+correct : "D"
+    },
+    {
+question : "Which of the following can't be done with client-side JavaScript?",
+choiceA : "validating a form",
+choiceB : "sending a form's content by email", 
+choiceC : "storing the form's contents to a database file on the server",
+choiceD : "none the above",
+correct : "C"
+    },
+    {
+question : "Which of the following are capabilities of functions in JavaScript",
+choiceA : "accept parameters and return a value",
+choiceB : "accept parameters", 
+choiceC : "return a value",
+choiceD : "none the above",
+correct : "B"
+    }
+];
